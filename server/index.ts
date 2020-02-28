846fed5c1c8f2d224e014c146da38c38e7cd8bbd
Fotso Teketchuet Stéphane <lesauf@gmail.com>
2020-02-28 23:36:30 +0100

    Working on testing backend

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+const express = require('express');
+// import * as express from 'express';
 // import express = require('express');
 
 import { routes } from './routes';
--- server/routes.ts
+++ server/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+const express = require('express');
 
 export const routes = express.Router();
 
